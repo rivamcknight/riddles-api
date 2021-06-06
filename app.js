0000883e7c86395334b1f4e1c8d0d42ba4a098fc
rivamcknight <45988124+rivamcknight@users.noreply.github.com>
2021-06-06 14:25:42 -0700

    untrack node_modules and add gitignore file, update app for heroku deployment

--- app.js
+++ app.js
@@ -209,7 +209,10 @@ app.get("/riddles/:riddleCategory/random", function(req, res) {
     });
 });
 
-const port = process.env.PORT || 3000;
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 app.listen(port, function() {
     console.log("Server started on port " + port);
 });
